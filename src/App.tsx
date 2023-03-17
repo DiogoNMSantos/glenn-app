1553ede4f747b7f99400157f28d01464ca7dea05
Diogo Nogal Santos <diogo.nogal.santos@gmail.comm>
2023-03-17 13:04:52 +0100

    chore: clean up project

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,15 @@
+import abcjs from "abcjs";
+import { useEffect } from "react";
+
 export function App() {
+	useEffect(() => {
+		abcjs.renderAbc("paper", "X:1\nK:D\nDD AA|BBA2|\n");
+	});
+
 	return (
 		<div className="App">
 			<h1>Gleen App</h1>
+			<div id="paper"></div>
 		</div>
 	);
 }
